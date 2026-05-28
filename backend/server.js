d1dfeb606e120df21af70c7ea2c551dab8132a30
Akshaya Vasireddi <vasireddiakshaya@gmail.com>
2026-05-28 16:49:07 +0000

    fix: prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,7 @@
-
-
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
 const connectDB = require("./config/db");
-
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 
@@ -17,10 +13,12 @@ connectDB();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
-// 🔥 DEBUG MIDDLEWARE (added by us)
+// 🔥 DEBUG MIDDLEWARE
 app.use((req, res, next) => {
   console.log(`➡️ ${req.method} ${req.url}`);
   next();
@@ -29,6 +27,7 @@ app.use((req, res, next) => {
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
+
 console.log("TASK ROUTES LOADED");
 
 // Test route
@@ -36,10 +35,8 @@ app.get("/", (req, res) => {
   res.send("Task Manager API Running");
 });
 
-// Port
+// Start Server
 const PORT = process.env.PORT || 5000;
-
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
