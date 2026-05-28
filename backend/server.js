a95f8ef8c4f4decac4a6453335870e1ef1caa082
Akshaya Vasireddi <vasireddiakshaya@gmail.com>
2026-05-28 17:22:11 +0000

    fix: update cors with correct vercel url

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: "https://task-manager-card.vercel.app"
+  origin: "https://task-manager-card-git-main-akshaya-vasireddi-s-projects.vercel.app"
 }));
 app.use(express.json());
 
